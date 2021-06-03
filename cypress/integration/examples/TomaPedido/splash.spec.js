99d5eb158bf9f5700311685271e14b5b0cba899d
Eugenio Braidot <eb.iot.developer@gmail.com>
2021-06-03 20:59:50 -0300

    Se agrega la modalidad de utilizacion Fixture y JSON para ir optimizando las llamadas e ir encapsulando los componentes

--- cypress/integration/examples/TomaPedido/splash.spec.js
+++ cypress/integration/examples/TomaPedido/splash.spec.js
@@ -1,40 +1,19 @@
-describe('SplashTest',() => {
-    it('Acceso a Pagina', () => {
-      cy.visit("/");
-      cy.on('uncaught:exception', (err, runnable) => {
-        console.log(err);
-        return false;
-      })
+describe('SplashTest', () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.on('uncaught:exception', (err, runnable) => {
+      console.log(err);
+      return false;
     })
-    // it('Titulo',()=>{
-    //     cy.get('.MuiTypography-root').contains('Bienvenido')
-    // })
-    // it('Login Box',()=>{
-    //     cy.get('img').click();
-    // })
-    // it('Ingreso de Cliente',()=>{
-    //     cy.get('.MuiInputBase-input').type('120104325{enter}');
-    // })
-    // it('Buscar Producto',() => {
-    //     cy.get('.makeStyles-paper-13 > .MuiGrid-container > .MuiGrid-root > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type('186')
-    // })
-    // it('Seleccion de Producto en lista',()=>{
-    //     cy.get('.MuiTableBody-root > .MuiTableRow-root > :nth-child(1)').click()
-    // })
-    // it('Agrego Unidades',()=>{
-    //     cy.get(':nth-child(2) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type('100{enter}')
-    // })
-    // it('Buscar Producto2',() => {
-    //     cy.get('.makeStyles-paper-13 > .MuiGrid-container > .MuiGrid-root > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').clear()
-    //     cy.get('.makeStyles-paper-13 > .MuiGrid-container > .MuiGrid-root > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type('975{enter}')
-    // })
-    // it('Seleccion de Producto en lista2',()=>{
-    //     cy.get('.MuiTableBody-root > .MuiTableRow-root > :nth-child(1)').click()
-    // })
-    // it('Agrego Unidades2',()=>{
-    //     cy.get(':nth-child(2) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type('1000{enter}')
-    // })
-    // it('Click on Ver Detalle',()=>{
-    //     cy.get('a > .MuiButtonBase-root > .MuiButton-label').click()
-    // })
+  });
+  it('Ingreso de Producto', () => {
+    cy.fixture('pagesElements').then((element) => {
+      cy.get(element.splash.name).should('contain', element.splash.value);
+      cy.get(element.splash.logoBox).click();
+      cy.get(element.client.code).type('120104325{enter}');
+      cy.get(element.client.inputSearch).type('186');
+      cy.get(element.client.ProductSelect).click();
+      cy.get(element.client.addUnits).type('100{enter}')
+    })
+  })
 })
