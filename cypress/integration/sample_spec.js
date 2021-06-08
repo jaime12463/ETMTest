fc2454e93b77f95698a57a3cbba0e3b04c704a26
Diego Gonzalez <diego.gonzalez@hasar.com>
2021-06-08 20:48:23 -0300

    Se escribe primer test de validacion para verificar compilacion de aplicacion

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,5 @@
+describe("Primeros Tests", () => {
+  it("Ingresando a la web", () => {
+    cy.visit("http://localhost:3000/");
+  });
+});
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,2 @@
-export const URL_API: string = process.env.REACT_APP_API_URL?? "http://localhost:4000";
+export const URL_API: string =
+  process.env.REACT_APP_API_URL ?? "http://localhost:4000";
