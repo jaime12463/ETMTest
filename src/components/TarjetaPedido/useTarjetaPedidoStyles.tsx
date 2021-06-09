98f33ad6f2e1e96758e059a44b66cbcab228459e
HSNBDES33\eocariz <eocariz@hasar.com>
2021-06-09 09:49:24 -0300

    Corrección azurePipelines

---
+++ src/components/TarjetaPedido/useTarjetaPedidoStyles.tsx
@@ -0,0 +1,32 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+      marginTop: 20,
+    },
+    bullet: {
+      display: "inline-block",
+      margin: "0 2px",
+      transform: "scale(0.8)",
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+    alignment: {
+      justifyContent: "center",
+    },
+    sectionCardInfo: {
+      paddingBottom: "0px",
+    },
+    sectionButtonDetail: {
+      textDecoration: "none",
+    },
+  });
+
+  export {
+      useStyles
+  }
