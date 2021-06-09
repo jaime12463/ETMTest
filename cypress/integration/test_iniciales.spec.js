f058c6d3ddbabb73ab7a9bdd1a87705d93a4fd5d
Diego Gonzalez <diego.gonzalez@hasar.com>
2021-06-08 22:43:26 -0300

    Se refactorizan test para que metodo visit se ejecute en beforeEach

--- cypress/integration/test_iniciales.spec.js
+++ cypress/integration/test_iniciales.spec.js
@@ -1,16 +1,13 @@
 describe("Primeros Tests", () => {
-  it("Ingresando a la web", () => {
-    cy.visit("http://localhost:3000/");
+  beforeEach(() => {
+    cy.visit("/");
   });
 
   it("Validar pantalla de bienvenida", () => {
-    cy.visit("http://localhost:3000/");
     cy.contains("Bienvenido");
   });
 
   it("Redireccionando a pantalla Ingreso Pedido", () => {
-    cy.visit("http://localhost:3000/");
-
     cy.get("[data-cy=boton-splash]").click();
 
     // Should be on a new URL which includes '/commands/actions'
@@ -18,19 +15,14 @@ describe("Primeros Tests", () => {
   });
 
   it("Validando escritura de cliente", () => {
-    cy.visit("http://localhost:3000/");
-
     cy.get("[data-cy=boton-splash]").click();
 
-    // Get an input, type into it and verify that the value has been updated
     cy.get(`[data-cy=codigo-cliente]`)
       .type("1234")
       .should("have.value", "1234");
   });
 
   it("Inicializando un pedido", () => {
-    cy.visit("http://localhost:3000/");
-
     cy.get("[data-cy=boton-splash]").click();
     cy.get(`[data-cy=codigo-cliente]`).type("120104325{enter}");
     cy.get("[data-cy=codigo-producto]").type("1860");
