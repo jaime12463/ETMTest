1b4731e188bacc151eb092cbcf56139b25966250
HSNBDES33\eocariz <eocariz@hasar.com>
2021-06-09 16:22:21 -0300

    vamos que sale

---
+++ features-output-d/pickledFeatures.js
@@ -0,0 +1,23 @@
+jsonPWrapper ({
+  "Features": [],
+  "Summary": {
+    "Tags": [],
+    "Folders": [],
+    "NotTestedFolders": [],
+    "Scenarios": {
+      "Total": 0,
+      "Passing": 0,
+      "Failing": 0,
+      "Inconclusive": 0
+    },
+    "Features": {
+      "Total": 0,
+      "Passing": 0,
+      "Failing": 0,
+      "Inconclusive": 0
+    }
+  },
+  "Configuration": {
+    "GeneratedOn": "9 junio 2021 16:09:01"
+  }
+});
