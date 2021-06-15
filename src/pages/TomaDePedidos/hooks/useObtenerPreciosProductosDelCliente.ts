92facc90ddb728509daf60df0f9998aac905784c
eivazquez <enrique.vazquez@hasar.com>
2021-06-15 14:30:20 -0300

    refactor implementando filter y map

--- src/pages/TomaDePedidos/hooks/useObtenerPreciosProductosDelCliente.ts
+++ src/pages/TomaDePedidos/hooks/useObtenerPreciosProductosDelCliente.ts
@@ -1,31 +1,26 @@
 import { useCallback } from "react";
 import { useAppSelector, useAppDispatch } from "redux/hooks";
 import { selectDatos } from "redux/features/datos/datosSlice";
-import { TPreciosProductos, TCliente } from "models";
+import { TPreciosProductos, TCliente, TPortafolio } from "models";
 import { validarFechaVigenciaProducto } from "utils/validaciones";
 
 export const useObtenerPreciosProductosDelCliente = () => {
   const { datos } = useAppSelector(selectDatos);
   const obtenerPreciosProductosDelCliente = useCallback(
     (clienteEncontrado: TCliente): TPreciosProductos => {
-      const preciosProductosDelCliente: TPreciosProductos = [];
-      clienteEncontrado.portafolio.forEach((productoPortafolio) => {
-        const productoEncontrado =
-          datos.productos[productoPortafolio.codigoProducto];
-        if (
-          productoEncontrado &&
-          validarFechaVigenciaProducto(
-            productoPortafolio.precios,
-            clienteEncontrado.fechasEntrega
-          )
-        ) {
-          preciosProductosDelCliente.push({
-            ...productoPortafolio,
-            nombre: productoEncontrado.nombre,
-            presentacion: productoEncontrado.presentacion,
-          });
-        }
-      });
+      const preciosProductosDelCliente: TPreciosProductos = clienteEncontrado.portafolio.filter(
+        (producto) =>  
+        { 
+            if (validarFechaVigenciaProducto( producto.precios,clienteEncontrado.fechasEntrega)) return producto; 
+        }).map(
+                (productoFiltrado) => 
+                {
+                    return {
+                    ...productoFiltrado, 
+                    "nombre": datos.productos[productoFiltrado.codigoProducto].nombre, 
+                    "presentacion": datos.productos[productoFiltrado.codigoProducto].presentacion };
+                }
+        );
       return preciosProductosDelCliente;
     },
     [datos]
