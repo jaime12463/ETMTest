3344cae445602b2788dd14717527a478a088e168
Jonatan Mancera <alexander.mancera@hasar.com>
2021-07-15 17:23:45 -0300

    Se agregar color principal y secundario igual al rojo de femsa.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import Rutas from "components/Rutas";
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#f33737"
+    },
+    secondary: {
+      main: "#f33737"
+    }
+  }
+});
 function App() {
   return (
     <Provider store={store}>
-      <Rutas />
+      <ThemeProvider theme={theme}>
+        <Rutas />
+      </ThemeProvider>
     </Provider>
   );
 }
