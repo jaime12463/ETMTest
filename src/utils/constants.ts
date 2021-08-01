75eeac1d5c4eff13b5b7d54a1b9bb26057874667
Jonatan Mancera <alexander.mancera@hasar.com>
2021-08-01 10:52:58 -0300

    Se arregla conflicto.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,8 @@
 export const URL_API: string =
 	process.env.REACT_APP_API_URL ?? 'http://localhost:4000';
+
+export const coloresBarra: any = {
+	verde: 'rgba(102,187,106,1)',
+	amarillo: ' rgba(255,204,128,1)',
+	rojo: ' rgba(229,57,53,1)',
+};
