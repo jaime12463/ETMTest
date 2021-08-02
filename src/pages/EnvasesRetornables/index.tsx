a3e7cd76c1fe5bc208b961c8b53ac4c2c0964a35
HSNBRUT11\Emmanuel Guzman <guzman-emmanuel@live.com>
2021-08-02 17:26:19 -0300

    elimino estilos propios en envases retornalbes

--- src/pages/EnvasesRetornables/index.tsx
+++ src/pages/EnvasesRetornables/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 import {Estructura} from 'components/UI';
-import {useAppSelector} from 'redux/hooks';
-import useEstilos from './useEstilos';
 import {ListadoEnvasesRetornables} from './components';
 
 const EnvasesRetornables: React.FC = () => {
-	const estilos = useEstilos();
 	const {t} = useTranslation();
 
 	return (
