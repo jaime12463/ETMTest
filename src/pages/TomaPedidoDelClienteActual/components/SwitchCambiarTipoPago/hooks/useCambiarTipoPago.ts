47fe263861fdca20fd4cb6d345bd7d976bf77e7a
Jonatan Mancera <alexander.mancera@hasar.com>
2021-08-06 15:34:48 -0300

    Se agrega en el slice pedidoActual reducer para cambiar el tipo de pago de un producto.

--- src/pages/TomaPedidoDelClienteActual/components/SwitchCambiarTipoPago/hooks/useCambiarTipoPago.ts
+++ src/pages/TomaPedidoDelClienteActual/components/SwitchCambiarTipoPago/hooks/useCambiarTipoPago.ts
@@ -1,20 +1,21 @@
 import {ETiposDePago, TProductoPedido} from 'models';
-import {agregarProductoAlPedidoDelCliente} from 'redux/features/pedidoActual/pedidoActualSlice';
+import {cambiarTipoPagoPoducto} from 'redux/features/pedidoActual/pedidoActualSlice';
 import {useAppDispatch} from 'redux/hooks';
 
 export const useCambiarTipoPago = () => {
 	const dispatch = useAppDispatch();
 	const cambiarTipoPago = (producto?: TProductoPedido) => {
+		//se debe tener algo que general indique en que esta el swicht principal,
+		//asi tambien los nuevos productos respetan eso
 		if (producto) {
-			const productoActual = {...producto};
-
-			const tipoDePagoCambio =
-				productoActual.tipoPago === ETiposDePago.Contado
+			const tipoPago =
+				producto.tipoPago === ETiposDePago.Contado
 					? ETiposDePago.Credito
 					: ETiposDePago.Contado;
-			productoActual.tipoPago = tipoDePagoCambio;
 
-			dispatch(agregarProductoAlPedidoDelCliente(productoActual));
+			const {codigoProducto} = producto;
+
+			dispatch(cambiarTipoPagoPoducto({codigoProducto, tipoPago}));
 			return;
 		}
 	};
