0802f4b043c7d5f6140e61e6e4cc9dcc7ca036f3
alonxx <alonxx123@gmail.com>
2021-08-30 12:37:01 -0300

    Finalizacion task 7345

--- src/pages/CompromisoDeCobro/components/index.tsx
+++ src/pages/CompromisoDeCobro/components/index.tsx
@@ -2,9 +2,13 @@ import ListadoDocumentos from './ListadoDocumentos';
 import ItemListadoDocumentos from './ItemListadoDocumentos';
 import InputAgregarCompromisoDeCobro from './InputAgregarCompromisoDeCobro';
 import TotalesCompromisoDeCobroPedidoActual from './TotalesCompromisoDeCobroPedidoActual';
+import ItemTarjetaPromoPush from './ItemTarjetaPromoPush';
+import TarjetasPromoPush from './TarjetasPromoPush';
 
 export {
 	ListadoDocumentos,
+	ItemTarjetaPromoPush,
+	TarjetasPromoPush,
 	ItemListadoDocumentos,
 	InputAgregarCompromisoDeCobro,
 	TotalesCompromisoDeCobroPedidoActual,
