3c27439fbf2b189b9449288db09474b2b4b76048
alonxx <alonxx123@gmail.com>
2021-09-02 13:47:16 -0300

    fix

--- src/hooks/useObtenerMostrarPromoPush.ts
+++ src/hooks/useObtenerMostrarPromoPush.ts
@@ -2,7 +2,6 @@ import {useAppSelector} from 'redux/hooks';
 import {selectVisitaActual} from 'redux/features/visitaActual/visitaActualSlice';
 
 export const useObtenerMostrarPromoPush = () => {
-	const prueba = 0;
 	const {mostrarPromoPush} = useAppSelector(selectVisitaActual);
 
 	return mostrarPromoPush;
