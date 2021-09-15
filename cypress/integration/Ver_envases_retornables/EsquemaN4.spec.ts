c42921a5b6a6be15502ac50818d5c396a9ed61df
HSNBRUT11\Emmanuel Guzman <guzman-emmanuel@live.com>
2021-09-15 10:11:55 -0300

    escenario 4 de envases retornables

--- cypress/integration/Ver_envases_retornables/EsquemaN4.spec.ts
+++ cypress/integration/Ver_envases_retornables/EsquemaN4.spec.ts
@@ -1,14 +1,17 @@
 import {Cuando, Entonces, Dado, Y} from '../../pasos';
 
 Dado('que se realizó la venta {int} de un producto Promo Push {int}', (cantidadUnidades, codigoPromo) => {
-
+	cy.navegarPageInicio('2021-06-09');
+	cy.esperarDatosServidor();
+	cy.oprimirBotonSplash();
+	cy.ingresarCodigoCliente('HS001');
+    cy.agregarProducto({
+		codigoProducto: codigoPromo,
+		unidades: cantidadUnidades,
+	});
 });
 
-Y('quiero ver los envases retornables del pedido', () => {
-
-});
-
-
-Entonces('el sistema mostrará', () => {
-    
+Entonces('el sistema mostrará lo siguiente', () => {
+    cy.get('[data-cy=listado-envases-retornables-0]').should('exist');
+    cy.get('[data-cy=listado-envases-retornables-1]').should('exist');
 });
--- cypress/integration/Ver_envases_retornables/EsquemaN5.spec.ts
+++ cypress/integration/Ver_envases_retornables/EsquemaN5.spec.ts
@@ -1,7 +1,10 @@
 import {Entonces, Dado} from '../../pasos';
 
 Dado('que se capturó el siguiente pedido', () => {
-
+	cy.navegarPageInicio('2021-06-09');
+	cy.esperarDatosServidor();
+	cy.oprimirBotonSplash();
+	cy.ingresarCodigoCliente('HS001');
 });
 
 Entonces('el sistema mostrará', () => {
