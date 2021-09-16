6039dbe45a6c2ddc64508dfd2ee6ea8a85e74d18
HSNBRUT11\Emmanuel Guzman <guzman-emmanuel@live.com>
2021-09-16 10:48:15 -0300

    subo fix de bug de envases

--- src/pages/EnvasesRetornables/components/ListadoEnvasesRetornables/index.tsx
+++ src/pages/EnvasesRetornables/components/ListadoEnvasesRetornables/index.tsx
@@ -1,24 +1,37 @@
 import {List} from 'components/UI';
 import {FunctionComponent} from 'react';
-import {THeader} from 'models';
+import {THeader, TProductoPedido} from 'models';
 import {Box} from '@material-ui/core';
 import {TConsolidadoImplicitos} from 'models';
 import {useObtenerConsolidacionImplicitos} from './hooks';
-import {useObtenerPedidoActual} from 'redux/hooks';
+import {useObtenerVisitaActual, useObtenerConfiguracion} from 'redux/hooks';
 import {ItemListadoEnvasesRetornables} from '..';
 import {useTranslation} from 'react-i18next';
 
 type Props = {};
 
 const ListadoEnvasesRetornables: FunctionComponent<Props> = (props) => {
-	const {productos} = useObtenerPedidoActual();
-	//TODO: Los envases se calculan respecto a que tipo de pedido?
+	const visitaActual = useObtenerVisitaActual();
+	let pedidosArray: TProductoPedido[] = [];
+	const {tipoPedidos} = useObtenerConfiguracion();
 
 	const obtenerConsolidacionImplicitos = useObtenerConsolidacionImplicitos();
 	//	TODO: La ejecución de obtenerConsolidacionImplicitos tiene que estar dentro de un useEffect
 	//  si lo que se quiere es que se realice solo en el primer render (Como esta se hace cada render)
+
+	Object.values(visitaActual.pedidos).forEach((pedido) => {
+		let esGeneraEnvases = false;
+		tipoPedidos.forEach((tipoPedido) => {
+			if(tipoPedido.codigo === pedido.tipoPedido)
+				esGeneraEnvases = tipoPedido.generaEnvases;
+		});
+
+		if(esGeneraEnvases)
+			pedidosArray = pedidosArray.concat(pedido.productos);
+	});
+
 	const consolidacionImplicitos: TConsolidadoImplicitos[] = obtenerConsolidacionImplicitos(
-		productos
+		pedidosArray
 	);
 
 	const Header = ({title}: {title: string}) => (
