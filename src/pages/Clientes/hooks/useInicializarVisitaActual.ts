d9001db100bc28bf017411f549dc4b13802cb65d
Jonatan Mancera <alexander.mancera@hasar.com>
2021-09-26 20:48:27 -0300

    Fix inicializar tipo de pedido actual

--- src/pages/Clientes/hooks/useInicializarVisitaActual.ts
+++ src/pages/Clientes/hooks/useInicializarVisitaActual.ts
@@ -1,18 +1,19 @@
+import {useObtenerDatosCliente} from 'hooks';
 import {TDatosClientesProductos, TPedidos} from 'models';
 import {useCallback} from 'react';
 import {inicializarVisitaActual} from 'redux/features/visitaActual/visitaActualSlice';
-import {useAppDispatch, useObtenerConfiguracion,useObtenerDatos,useObtenerPedidosClientes} from 'redux/hooks';
+import {useAppDispatch, useObtenerConfiguracion} from 'redux/hooks';
 import {useInicializarPedidos} from '.';
-
 
 export const useInicializarVisitaActual = () => {
 	const dispatch = useAppDispatch();
 	const inicializarPedidos = useInicializarPedidos();
 	const configuracion = useObtenerConfiguracion();
-	
+	const {obtenerDatosCliente} = useObtenerDatosCliente();
 
 	const useInicializarPedidoActual = useCallback(
 		(fechaEntrega: string, codigoCliente: string) => {
+			const datosCliente = obtenerDatosCliente(codigoCliente);
 			const pedidos: TPedidos = inicializarPedidos(fechaEntrega, codigoCliente);
 
 			const tiposPedidos = configuracion.tipoPedidos
@@ -20,13 +21,18 @@ export const useInicializarVisitaActual = () => {
 				.sort((a, b) => a.secuencia - b.secuencia);
 
 			const tipoPedidoActual: number =
-				tiposPedidos.find((tipoPedido) => tipoPedido.esValorizado)?.codigo ??
-				tiposPedidos[0].codigo;
+				tiposPedidos.find(
+					(tipoPedido) =>
+						tipoPedido.esValorizado &&
+						datosCliente?.configuracionPedido.tipoPedidoHabilitados.includes(
+							tipoPedido.codigo
+						)
+				)?.codigo ?? tiposPedidos[0].codigo;
 
 			const mostrarPromoPush: boolean = false;
-		
-			const bloquearPanelCarga: boolean=false;
-			
+
+			const bloquearPanelCarga: boolean = false;
+
 			dispatch(
 				inicializarVisitaActual({
 					visitaActual: {
@@ -34,9 +40,9 @@ export const useInicializarVisitaActual = () => {
 						pedidos,
 						tipoPedidoActual,
 						mostrarPromoPush,
-						saldoPresupuestoTipoPedido:{},
+						saldoPresupuestoTipoPedido: {},
 						bloquearPanelCarga,
-						ordenDeCompra:'',
+						ordenDeCompra: '',
 					},
 				})
 			);
