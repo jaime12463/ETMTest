0bdef2216f6b935fbfd68486562fe811a223bab0
eivazquez <enrique.vazquez@hasar.com>
2021-09-30 09:14:03 -0300

    Se realiza correción de nombre de la barra de progreso

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -8,7 +8,7 @@ import TabPanel from './TabPanel';
 import FormInput from './FormInput';
 import Rutas from './Rutas';
 import Center from './Center';
-import BarraDeProgeso from './BarraDeProgreso';
+import BarraDeProgreso from './BarraDeProgreso';
 import Numero from './Numero';
 import Cajon from './Cajon';
 import BuscadorenLista from './BuscadorenLista';
@@ -29,7 +29,7 @@ export {
 	FormInput,
 	Rutas,
 	Center,
-	BarraDeProgeso,
+	BarraDeProgreso,
 	Numero,
 	Cajon,
 	BuscadorenLista,
