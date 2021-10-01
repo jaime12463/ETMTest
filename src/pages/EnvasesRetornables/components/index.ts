ab4886b6bd7a50d3d4933451ec1d10ffe8652dea
alonxx <alonxx123@gmail.com>
2021-10-01 12:45:17 -0300

    Correcion Promopush

--- src/pages/EnvasesRetornables/components/index.ts
+++ src/pages/EnvasesRetornables/components/index.ts
@@ -1,11 +1,4 @@
-import ListadoEnvasesRetornables from './ListadoEnvasesRetornables';
-import ItemListadoEnvasesRetornables from './ItemListadoEnvasesRetornables';
 import ContenedorEnvasesRetornables from './ContenedorEnvasesRetornables';
-import TarjetaEnvasesRetornables from './TarjetaEnvasesRetornables'
+import TarjetaEnvasesRetornables from './TarjetaEnvasesRetornables';
 
-export {
-	ListadoEnvasesRetornables,
-	ItemListadoEnvasesRetornables,
-	ContenedorEnvasesRetornables,
-	TarjetaEnvasesRetornables,
-};
+export {ContenedorEnvasesRetornables, TarjetaEnvasesRetornables};
