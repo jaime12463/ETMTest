bf6d45ee548fdea40167a7def3d9d9032cc162c8
Nicolas <nicolas.santuccio@hasar.com>
2021-10-18 19:39:01 -0300

    Corrección tamaño de fuente del autocomplete

--- src/components/Negocio/AutocompleteSeleccionarProducto/useEstilos.ts
+++ src/components/Negocio/AutocompleteSeleccionarProducto/useEstilos.ts
@@ -1,19 +1,15 @@
 import {makeStyles} from '@material-ui/styles';
 
 const useEstilos = makeStyles(() => ({
+	root: {
+		'& .MuiInput-root .MuiInput-input': {
+			fontSize: '12px',
+		},
+	},
 	cajaAutocomplete: {
-		//background: '#FFFFFF',
 		boxShadow: '0px 2px 15px rgba(0, 0, 0, 0.15)',
 		borderRadius: '20px',
 		border: '100px',
-	},
-	textSeleccionar: {
-		// fontFamily: 'Open Sans',
-		// fontStyle: 'normal',
-		// fontWeight: 'normal',
-		// fontSize: '12px',
-		// lineHeight: '16px',
-		// color: '#B2B2B2',
 	},
 }));
 
