9cf63fa687d8d26de84f43e01ef23afbd168fa28
Nicolas <nicolas.santuccio@hasar.com>
2021-10-18 14:00:29 -0300

    Modificaciones tarjeta y estilos del switch

--- src/pages/Pasos/2_TomaDePedido/components/SwitchCambiarTipoPago/useEstilos.tsx
+++ src/pages/Pasos/2_TomaDePedido/components/SwitchCambiarTipoPago/useEstilos.tsx
@@ -1,26 +1,25 @@
 import {makeStyles} from '@material-ui/styles';
 
 export interface SwitchProps {
-  content: boolean
+	content: boolean;
 }
 
-const useEstilos = makeStyles(() => ({  
-  track: {
-    backgroundColor: "#FF0000",
-    opacity: "1",
-    "&:after, &:before": {
-      color: "white",
-      fontFamily: 'Open Sans',
-      fontSize: "10px",
-      position: "absolute",
-      top: "6px"
-    },
-    "&:before": {
-      content: (props: SwitchProps) => props.content ? "'Crédito'" : "'Contado'",
-      right: (props: SwitchProps) => props.content ? "22px" : '12px',
-      top: "1px",
-    }
-  },
+const useEstilos = makeStyles(() => ({
+	track: {
+		'&:after, &:before': {
+			color: 'white',
+			fontFamily: 'Open Sans',
+			fontSize: '10px',
+			position: 'absolute',
+			top: '6px',
+		},
+		'&:before': {
+			content: (props: SwitchProps) =>
+				props.content ? "'Crédito'" : "'Contado'",
+			right: (props: SwitchProps) => (props.content ? '22px' : '12px'),
+			top: '1px',
+		},
+	},
 }));
 
 export default useEstilos;
