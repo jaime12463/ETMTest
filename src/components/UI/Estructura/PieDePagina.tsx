dd4179486ea54f0484c11a286b43ff6548f60bc1
Nicolas <nicolas.santuccio@hasar.com>
2021-10-19 13:24:29 -0300

    Arreglado el ancho de la app

--- src/components/UI/Estructura/PieDePagina.tsx
+++ src/components/UI/Estructura/PieDePagina.tsx
@@ -1,13 +1,18 @@
+import React from 'react';
 import {Box, Container} from '@mui/material';
+import useEstilos from './useEstilos';
 
-export type Props = {
-	children: React.ReactNode;
-};
+const PieDePagina: React.FC = ({children}) => {
+	const estilos = useEstilos();
 
-const PieDePagina = ({children}: Props) => {
 	return (
 		<Box display='flex' justifyContent='center'>
-			<Container maxWidth='xs' component='footer' disableGutters={true}>
+			<Container
+				maxWidth='xs'
+				component='footer'
+				disableGutters={true}
+				classes={{maxWidthXs: estilos.maxWidthXs}}
+			>
 				<Box p={1}>{children}</Box>
 			</Container>
 		</Box>
