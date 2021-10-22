5a3a0a4d31a0d8c14df14c8ad1c128c0037ba68c
eguzman_hasar <emmanuel.guzman@hasar.com>
2021-10-22 16:56:57 -0300

    Subo fix envases en subunidades quedan erroneos

---
+++ src/pages/Pasos/3_Otros/EnvasesRetornables/components/TarjetaEnvasesRetornables/components/TarjetaDobleIzquierda/index.tsx
@@ -0,0 +1,100 @@
+import { FunctionComponent } from "react";
+import {Box, Chip, Typography} from '@mui/material';
+import {formatearNumero} from 'utils/methods';
+import botella from 'assests/iconos/botella.svg';
+import caja from 'assests/iconos/caja.svg';
+import { TConsolidadoImplicitos } from "models";
+import { useTranslation } from "react-i18next";
+import {styled} from '@mui/material/styles';
+import {ETiposDePago} from 'models';
+
+type Props = {
+    envase: TConsolidadoImplicitos,
+    tieneTipoPedidoValorizado: boolean,
+};
+
+const ChipStyled = styled(Chip)(({theme}) => ({
+	textAlign: 'center',
+	fontFamily: 'Open Sans',
+	width: '70px',
+	height: '18px',
+	padding: 0,
+}));
+
+
+const TarjetaDobleIzquierda: FunctionComponent<Props> = (props) => {
+
+    const {
+        envase,
+        tieneTipoPedidoValorizado,
+    } = props;
+
+    const {t} = useTranslation();
+
+    return (
+        <>
+            <Box p={1.5} pb={0} minWidth={'304px'} minHeight={'125px'}>
+                <Box
+                    style={{
+                        visibility:
+                            envase.tipoPago === undefined ? 'hidden' : 'visible',
+                    }}
+                    sx={{
+                        width: '80px',
+                        height: '16px',
+                        marginBottom: 2,
+                    }}
+                >
+                    {envase.tipoPago !== undefined && (
+                        <ChipStyled
+                            label={
+                                <Typography
+                                    variant={'caption'}
+                                    color='white'
+                                    textAlign='center'
+                                    p={0}
+                                >
+                                    {ETiposDePago[envase.tipoPago]}
+                                </Typography>
+                            }
+                            color={envase.tipoPago === 1 ? 'success' : 'primary'}
+                        />
+                    )}
+                </Box>
+                <Typography fontFamily='Open Sans' variant={'subtitle2'}>
+                    {envase.codigoImplicito}
+                </Typography>
+                <Typography variant={'subtitle2'}>
+                    {envase.nombreImplicito}
+                </Typography>
+                <br />
+                <Box display='flex' alignItems='center' gap='4px'>
+                    {tieneTipoPedidoValorizado && (
+                        <>
+                            <img style={{width: '19px'}} src={caja} alt='icono caja' />
+                            <Typography variant={'caption'}>
+                                {`x${envase.presentacion} `}
+                            </Typography>
+                            <Typography variant={'subtitle3'}>
+                                {envase.precioConImpuestoUnidad &&
+                                    formatearNumero(envase.precioConImpuestoUnidad, t)}
+                            </Typography>
+
+                            <img
+                                style={{width: '19px'}}
+                                src={botella}
+                                alt='icono botella'
+                            />
+                            <Typography variant={'subtitle3'}>
+                                {envase.precioConImpuestoSubunidad &&
+                                    formatearNumero(envase.precioConImpuestoSubunidad, t)}
+                            </Typography>
+                        </>
+                    )}
+                </Box>
+            </Box>
+        </>
+    )
+}
+
+export default TarjetaDobleIzquierda;
