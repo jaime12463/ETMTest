d6b2080724ba76c28ce58d575afbf80c02682171
Nicolas <nicolas.santuccio@hasar.com>
2022-01-06 10:05:17 -0300

    Correcciones UI Resumen de pedido - Promociones

--- src/components/UI/ResumenPedido/Resumen/TituloPromo/index.tsx
+++ src/components/UI/ResumenPedido/Resumen/TituloPromo/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import {PromocionesRellenoIcon} from 'assests/iconos';
 import theme from 'theme';
 
 export const TituloPromo: React.FC = () => {
@@ -9,12 +8,10 @@ export const TituloPromo: React.FC = () => {
 		<Box
 			alignItems='center'
 			display='flex'
-			gap='4px'
 			justifyContent='center'
-			padding='10px 0'
+			padding='12px 0'
 			sx={{background: theme.palette.primary.main}}
 		>
-			<PromocionesRellenoIcon height='17px' width='17px' />
 			<Typography variant='subtitle3' color='#fff'>
 				Promo push
 			</Typography>
