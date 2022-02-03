9fa1031127da08c40dc0d755b801504d01a4374f
eguzman_hasar <emmanuel.guzman@hasar.com>
2022-02-03 17:39:42 -0300

    agrego calculo de envases para obsequio

--- src/pages/Pasos/3_Otros/EnvasesRetornables/components/ContenedorEnvasesRetornables/index.tsx
+++ src/pages/Pasos/3_Otros/EnvasesRetornables/components/ContenedorEnvasesRetornables/index.tsx
@@ -1,23 +1,20 @@
-import {TarjetaColapsable} from 'components/UI';
-import {FunctionComponent, useState} from 'react';
-import {TProductoPedido, ETiposDePago} from 'models';
-import {Stack, Typography} from '@mui/material';
+import {FunctionComponent} from 'react';
+import {TProductoPedido} from 'models';
+import {Stack} from '@mui/material';
 import {TConsolidadoImplicitos} from 'models';
-import {useObtenerConsolidacionImplicitos} from './hooks';
+import {
+	useObtenerConsolidacionImplicitos,
+	useObtenerDatosProducto,
+} from './hooks';
 import {useObtenerVisitaActual, useObtenerConfiguracion} from 'redux/hooks';
 import TarjetaEnvasesRetornables from '../TarjetaEnvasesRetornables';
-import {borrarProductosDeVisitaActual} from 'redux/features/visitaActual/visitaActualSlice';
 import Box from '@mui/material/Box';
 
-type Props = {};
-
-const ContenedorEnvasesRetornables: FunctionComponent<Props> = (props) => {
-	const [expandido, setExpandido] = useState<string | boolean>(false);
-
+const ContenedorEnvasesRetornables: FunctionComponent = () => {
 	const visitaActual = useObtenerVisitaActual();
-
 	const {tipoPedidos} = useObtenerConfiguracion();
 	const obtenerConsolidacionImplicitos = useObtenerConsolidacionImplicitos();
+	const obtenerDatosProducto = useObtenerDatosProducto();
 
 	let pedidosArray: TProductoPedido[] = [];
 	let esGeneraEnvases = false;
@@ -34,6 +31,64 @@ const ContenedorEnvasesRetornables: FunctionComponent<Props> = (props) => {
 
 		if (esGeneraEnvases) pedidosArray = pedidosArray.concat(pedido.productos);
 	});
+
+	const calcularEnvasesDeObsequios = () => {
+		const medidaUnidad = 'Unidad';
+		const medidaSubunidad = 'Subunidad';
+
+		let promosConvertidasAProducto: TProductoPedido[] = [];
+		visitaActual.promosOngoing.forEach((promo) => {
+			promo.productos.forEach((producto) => {
+				const {implicito1, implicito2} = obtenerDatosProducto(
+					producto.codigoProducto
+				);
+				let datosImplicito1,  datosImplicito2;
+
+				if (implicito1)
+					datosImplicito1 = obtenerDatosProducto(implicito1);
+				if (implicito2)
+					datosImplicito2 = obtenerDatosProducto(implicito2);					
+
+				const productoConvertido: TProductoPedido = {
+					codigoProducto: producto.codigoProducto,
+					unidades:
+						producto.unidadMedida === medidaUnidad ? producto.cantidad : 0,
+					subUnidades:
+						producto.unidadMedida === medidaSubunidad ? producto.cantidad : 0,
+					tipoPago: producto.tipoPago,
+					codigoImplicito1: implicito1,
+					nombreImplicito1: datosImplicito1 ? datosImplicito1.nombre : '',
+					codigoImplicito2: implicito2,
+					nombreImplicito2: datosImplicito2 ? datosImplicito2.nombre : '',
+
+					nombreProducto: '',
+					presentacion: 0,
+					subunidadesVentaMinima: 0,
+					esVentaSubunidades: false,
+					precioConImpuestoUnidad: 0,
+					precioConImpuestoSubunidad: 0,
+					tipoProducto: 0,
+					total: 0,
+					preciosBase: {
+						unidad: 0,
+						subUnidad: 0,
+					},
+					preciosNeto: {
+						unidad: 0,
+						subUnidad: 0,
+					},
+					catalogoMotivo: '',
+				};
+
+				promosConvertidasAProducto =
+					promosConvertidasAProducto.concat(productoConvertido);
+			});
+		});
+
+		return promosConvertidasAProducto;
+	};
+
+	pedidosArray = pedidosArray.concat(calcularEnvasesDeObsequios());
 
 	const consolidacionImplicitos: TConsolidadoImplicitos[] =
 		obtenerConsolidacionImplicitos(pedidosArray).sort((a, b) =>
