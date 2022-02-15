cc6620cbf66a4a1243c6eaf2eede72c997911263
Nicolas <nicolas.santuccio@hasar.com>
2022-02-15 10:29:12 -0300

    React Portals para modal

--- src/components/UI/ModalCore/index.tsx
+++ src/components/UI/ModalCore/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Box from '@mui/material/Box';
 import useEstilos from './useEstilos';
 
@@ -9,14 +10,20 @@ interface Props {
 
 const ModalCore: React.FC<Props> = ({open, borderRadius = false, children}) => {
 	const classes = useEstilos({borderRadius});
-	return (
+
+	const modalPortal = document.getElementById('modal');
+
+	if (!modalPortal) return null;
+
+	return ReactDOM.createPortal(
 		<>
 			{open && (
 				<Box className={classes.container}>
 					<Box className={classes.card}>{children}</Box>
 				</Box>
 			)}
-		</>
+		</>,
+		modalPortal
 	);
 };
 
