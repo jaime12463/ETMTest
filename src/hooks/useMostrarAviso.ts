24c9e78e63e114cbe9c2cd0615b5adbbcb860ead
Nicolas <nicolas.santuccio@hasar.com>
2022-05-23 17:26:03 -0300

    Debugger removido de useMostrarAviso

--- src/hooks/useMostrarAviso.ts
+++ src/hooks/useMostrarAviso.ts
@@ -1,11 +1,10 @@
-import { debug } from 'console';
 import {OptionsObject, useSnackbar} from 'notistack';
-import { useObtenerConfiguracion } from '../redux/hooks/useObtenerConfiguracion';
+import {useObtenerConfiguracion} from '../redux/hooks/useObtenerConfiguracion';
 
 export const useMostrarAviso = () => {
 	const {enqueueSnackbar} = useSnackbar();
 	const {tiempoToastEnSegundos} = useObtenerConfiguracion();
-debugger
+
 	const mostrarAviso = (
 		tipo: 'default' | 'error' | 'success' | 'warning' | 'info',
 		titulo: string,
@@ -15,7 +14,7 @@ debugger
 	) => {
 		const opcionesDefault: OptionsObject = {
 			preventDuplicate: true,
-			autoHideDuration: tiempoToastEnSegundos*1000,
+			autoHideDuration: tiempoToastEnSegundos * 1000,
 			anchorOrigin: {
 				vertical: 'top',
 				horizontal: 'center',
